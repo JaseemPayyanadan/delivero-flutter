0a36081db26e4100fc2a220bf54a7838581dbfc7
Jaseem UI <jaseem.p@pickcel.com>
2026-07-17 18:14:23 +0530

    feat(ios): update iOS project configuration and dependencies
    - Set minimum iOS deployment target to 15.0 in Podfile.
    - Added multiple build configurations for Debug, Profile, and Release modes, including dev and prod variants.
    - Updated Podfile.lock to include new dependencies and versions.
    - Enhanced Xcode project settings to accommodate new build configurations and frameworks.
    - Integrated Firebase services with updated versions in pubspec.yaml and pubspec.lock.
    - Added URL schemes for Google services in Info.plist.
    - Introduced new Xcode schemes for development and production environments.

--- lib/core/services/push_notification_service.dart
+++ lib/core/services/push_notification_service.dart
@@ -1,5 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart' hide FirebaseService;
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
